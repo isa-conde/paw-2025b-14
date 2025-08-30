7d9a6acf752661a219d507c67718dc96ea29cee8
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-08-30 17:26:54 -0300

    Revert "added oracle's ide to the gitignore"
    This reverts commit c73ce99828d91b3a2856227ca22636eacec8ed74.

--- persistence/bin/src/main/resources/db/initdb/schema.sql
+++ persistence/bin/src/main/resources/db/initdb/schema.sql
@@ -3,26 +3,9 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(100) NOT NULL UNIQUE
 );
 
-
 CREATE TABLE IF NOT EXISTS game (
         id BIGSERIAL PRIMARY KEY,
         name text NOT NULL UNIQUE,
-        genre VARCHAR(100)
-);
-
-
-CREATE TABLE IF NOT EXISTS tournament (
-        id SERIAL PRIMARY KEY,
-        creator_id INT NOT NULL,
-        name text NOT NULL,
-        game_id INT NOT NULL,
-        region region_enum,
-        elo elo_enum,
-        start_date date,
-        end_date date,
-        format TEXT NOT NULL,
-        structure structure_enum,
-        max_participants INT NOT NULL,
-        CONSTRAINT tournament_dates_check CHECK (start_date IS NULL OR end_date IS NULL OR start_date < end_date)
+        genre genre_enum
 );
 
