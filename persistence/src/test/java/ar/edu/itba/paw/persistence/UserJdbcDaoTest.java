71a05a9b3e6ee7681a452fa89963fbb48065f584
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-09-15 01:46:11 -0300

    started UserJdbcDao testing

--- persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
@@ -44,3 +44,50 @@
 //    }
 //
 //}
+package ar.edu.itba.paw.persistence;
+
+
+import ar.edu.itba.paw.model.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.sql.DataSource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Sql("classpath:db/init.sql")
+public class UserJdbcDaoTest {
+    private static final String USERNAME = "johndoe";
+    private static final String EMAIL = "some@mail.com";
+
+    @Autowired
+    private DataSource ds;
+
+    @Autowired
+    private UserJdbcDao userJdbcDao;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setUp(){
+        jdbcTemplate = new JdbcTemplate(ds);
+        JdbcTestUtils.deleteFromTables(jdbcTemplate,"users");
+    }
+
+    @Test
+    public void testCreate(){
+        final User user = userJdbcDao.create(USERNAME,EMAIL);
+        Assert.assertNotNull(user);
+        Assert.assertEquals(USERNAME,user.getUsername());
+        Assert.assertEquals(EMAIL, user.getEmail());
+        Assert.assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate,"users"));
+    }
+}
