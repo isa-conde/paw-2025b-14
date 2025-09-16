76c81247d7f431d85820da192c9fa4fa4daf1691
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-09-15 21:57:30 -0300

    started testing GameJdbcDao

--- persistence/src/test/resources/db/init.sql
+++ persistence/src/test/resources/db/init.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS users(
     email varchar(100) NOT NULL UNIQUE ,
     username varchar(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS game(
+    id INTEGER IDENTITY PRIMARY KEY ,
+    name varchar(100) NOT NULL UNIQUE ,
+    genre varchar(20) ,
+    image_id INTEGER
+);
