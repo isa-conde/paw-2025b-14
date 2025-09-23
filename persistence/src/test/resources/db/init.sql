d9858bee80a37eacf44aa7b0b39d97bc147ab2a7
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-09-23 01:02:46 -0300

    tried to test TournamentJdbcDao

--- persistence/src/test/resources/db/init.sql
+++ persistence/src/test/resources/db/init.sql
@@ -10,3 +10,27 @@ CREATE TABLE IF NOT EXISTS game(
     genre varchar(20) ,
     image_id INTEGER
 );
+
+CREATE TABLE IF NOT EXISTS tournament(
+    id INTEGER IDENTITY PRIMARY KEY ,
+    creator_id INTEGER NOT NULL ,
+    name varchar(50) NOT NULL ,
+    game_id INTEGER NOT NULL ,
+    region varchar(10) ,
+    elo varchar(10) ,
+    start_date date ,
+    end_date date ,
+    format varchar(100) NOT NULL ,
+    max_participants INTEGER NOT NULL ,
+    structure varchar(20) ,
+    image_id INTEGER ,
+    open_inscriptions BOOLEAN ,
+    is_finished BOOLEAN ,
+    CONSTRAINT tournament_dates_check
+        CHECK  ((start_date IS NULL) OR (end_date IS NULL) OR (start_date < end_date))
+);
+
+CREATE TABLE IF NOT EXISTS image(
+    id INTEGER IDENTITY PRIMARY KEY ,
+    image varbinary(1000000)
+)
