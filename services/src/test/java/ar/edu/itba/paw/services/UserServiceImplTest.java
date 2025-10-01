03da5f789926811eae3167c53b05569f1b6f0186
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-10-01 14:20:24 -0300

    UserServiceImpl tested up to date

--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -2,18 +2,22 @@ package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.exception.EmailAlreadyUsedException;
 import ar.edu.itba.paw.interfaces.exception.UsernameAlreadyUsedException;
+import ar.edu.itba.paw.interfaces.persistence.TokenDao;
 import ar.edu.itba.paw.interfaces.persistence.UserDao;
+import ar.edu.itba.paw.interfaces.services.MailService;
+import ar.edu.itba.paw.model.Token;
 import ar.edu.itba.paw.model.User;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,12 +25,20 @@ public class UserServiceImplTest{
     private static final String USERNAME="johndoe";
     private static final String EMAIL="some@mail.com";
     private static final String PASSWORD="1234567890";
+    private static final String SOME_URL = "url.com";
+    private final static int DAYS_DURATION = 1;
 
     @Mock
-    private UserDao mockDao;
+    private UserDao mockUserDao;
 
     @Mock
     private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private TokenDao mockTokenDao;
+
+    @Mock
+    private MailService mockMailService;
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -34,9 +46,9 @@ public class UserServiceImplTest{
     @Test
     public void testCreate(){
         Mockito.when(passwordEncoder.encode(PASSWORD)).thenReturn(PASSWORD);
-        Mockito.when(mockDao.checkUsernameExists(USERNAME)).thenReturn(false);
-        Mockito.when(mockDao.checkEmailExists(EMAIL)).thenReturn(false);
-        Mockito.when(mockDao.create(Mockito.eq(USERNAME),Mockito.eq(EMAIL),Mockito.eq(PASSWORD))).thenReturn(new User(1,USERNAME,EMAIL,PASSWORD, false));
+        Mockito.when(mockUserDao.checkUsernameExists(USERNAME)).thenReturn(false);
+        Mockito.when(mockUserDao.checkEmailExists(EMAIL)).thenReturn(false);
+        Mockito.when(mockUserDao.create(Mockito.eq(USERNAME),Mockito.eq(EMAIL),Mockito.eq(PASSWORD))).thenReturn(new User(1,USERNAME,EMAIL,PASSWORD, false));
 
         User maybeUser = userService.create(USERNAME,EMAIL,PASSWORD);
 
@@ -49,71 +61,132 @@ public class UserServiceImplTest{
 
     @Test(expected = UsernameAlreadyUsedException.class)
     public void testUsernameExists(){
-        Mockito.when(mockDao.checkUsernameExists(USERNAME)).thenReturn(true);
+        Mockito.when(mockUserDao.checkUsernameExists(USERNAME)).thenReturn(true);
 
         User maybeUser = userService.create(USERNAME,EMAIL,PASSWORD);
     }
 
     @Test(expected = EmailAlreadyUsedException.class)
     public void testEmailExists(){
-        Mockito.when(mockDao.checkUsernameExists(USERNAME)).thenReturn(false);
-        Mockito.when(mockDao.checkEmailExists(EMAIL)).thenReturn(true);
+        Mockito.when(mockUserDao.checkUsernameExists(USERNAME)).thenReturn(false);
+        Mockito.when(mockUserDao.checkEmailExists(EMAIL)).thenReturn(true);
 
         User maybeUser = userService.create(USERNAME,EMAIL,PASSWORD);
     }
 
+    // Los casos donde el mail no existe o el token generado está repetido aún no se contemplaron
     @Test
-    public void testFindById(){
-        Mockito.when(mockDao.findById(1)).thenReturn(Optional.of(new User(1, USERNAME, EMAIL,PASSWORD, false)));
+    public void testResetOrVerifyEmail(){
+        Mockito.when(mockUserDao.findByEmail(EMAIL))
+                .thenReturn(Optional.of(new User(1,USERNAME,EMAIL,PASSWORD, false)));
+        Mockito.when(mockTokenDao.findByToken(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.empty());
+        Mockito.when(mockTokenDao.create(ArgumentMatchers.eq(1L), ArgumentMatchers.anyLong(), ArgumentMatchers.eq(LocalDate.now().plusDays(DAYS_DURATION)) ))
+                .thenReturn(new Token(1L, 1L, 1L, LocalDate.now().plusDays(DAYS_DURATION)));
 
-        Optional<User> maybeUser = userService.findById(1);
+        userService.requestPasswordReset(EMAIL,SOME_URL);
 
-        Assert.assertNotNull(maybeUser);
-        Assert.assertTrue(maybeUser.isPresent());
-        Assert.assertEquals(USERNAME, maybeUser.get().getUsername());
-        Assert.assertEquals(EMAIL, maybeUser.get().getEmail());
-        Assert.assertEquals(PASSWORD,maybeUser.get().getPassword());
-        Assert.assertEquals(1L, maybeUser.get().getId());
-        Mockito.verify(mockDao).findById(1);
-        Mockito.verifyNoMoreInteractions(mockDao);
+        Mockito.verify(mockMailService).sendResetPasswordEmail(1L,1L,EMAIL,SOME_URL);
     }
 
     @Test
-    public void testFindNonExistent(){
-        Mockito.when(mockDao.findById(1)).thenReturn(Optional.empty());
+    public void testSameAsOldPassword(){
+        Mockito.when(mockUserDao.findById(1L))
+                .thenReturn(Optional.of(new User(1L,USERNAME,EMAIL,PASSWORD, false)));
+        Mockito.when(passwordEncoder.matches(PASSWORD,PASSWORD)).thenReturn(true);
 
-        Optional<User> maybeUser = userService.findById(1);
+        boolean ans = userService.sameAsOldPassword(PASSWORD,1L);
 
-        Assert.assertNotNull(maybeUser);
-        Assert.assertTrue(maybeUser.isEmpty());
-        Mockito.verify(mockDao).findById(1);
-        Mockito.verifyNoMoreInteractions(mockDao);
+        Assert.assertTrue(ans);
     }
 
+    @Test
+    public void testSameAsNoPassword(){
+        Mockito.when(mockUserDao.findById(1L))
+                .thenReturn(Optional.of(new User(1L,USERNAME,EMAIL,null, false)));
+
+        boolean ans = userService.sameAsOldPassword(PASSWORD,1L);
+
+        Assert.assertFalse(ans);
+    }
+
+    @Test
+    public void testNotTheOldPassword(){
+        Mockito.when(mockUserDao.findById(1L))
+                .thenReturn(Optional.of(new User(1L,USERNAME,EMAIL,PASSWORD, false)));
+        Mockito.when(passwordEncoder.matches(PASSWORD,PASSWORD)).thenReturn(false);
+
+        boolean ans = userService.sameAsOldPassword(PASSWORD,1L);
+
+        Assert.assertFalse(ans);
+    }
+
+    @Test
+    public void testCheckTokenValidity(){
+        Mockito.when(mockTokenDao.findByToken(1L))
+                .thenReturn(Optional.of(new Token(1L,1L,1L,LocalDate.now().plusDays(DAYS_DURATION))));
+
+        Optional<Token> ans = userService.checkTokenValidity(1L,1L);
+
+        Mockito.verify(mockTokenDao).markAsUsed(1L);
+        Assert.assertTrue(ans.isPresent());
+        Assert.assertEquals(Long.valueOf(1L), ans.get().getUser_id());
+        // con esto, se analiza que user dao recibe los userId correctos en resetPassword y verifyEmail
+    }
+
+    @Test
+    public void testNoTokenValidity(){
+        Mockito.when(mockTokenDao.findByToken(1L))
+                .thenReturn(Optional.empty());
+
+        Optional<Token> ans = userService.checkTokenValidity(1L,1L);
+
+        Assert.assertTrue(ans.isEmpty());
+    }
+
+    @Test
+    public void testWrongTokenValidity(){
+        Mockito.when(mockTokenDao.findByToken(1L))
+                .thenReturn(Optional.of(new Token(1L,2L,1L,LocalDate.now().plusDays(DAYS_DURATION))));
+
+        Optional<Token> ans = userService.checkTokenValidity(1L,1L);
+
+        Assert.assertTrue(ans.isEmpty());
+    }
+
+    @Test
+    public void testLateTokenValidity(){
+        Mockito.when(mockTokenDao.findByToken(1L))
+                .thenReturn(Optional.of(new Token(1L,1L,1L,LocalDate.now())));
+
+        Optional<Token> ans = userService.checkTokenValidity(1L,1L);
+
+        Assert.assertTrue(ans.isEmpty());
+    }
+
+    /// Redundante
 //    @Test
-//    public void testAuthenticate(){
-//        Mockito.when(mockDao.authenticate(USERNAME,EMAIL)).thenReturn(Optional.of(new User(1, USERNAME, EMAIL)));
+//    public void testFindById(){
+//        Mockito.when(mockUserDao.findById(1)).thenReturn(Optional.of(new User(1, USERNAME, EMAIL,PASSWORD, false)));
 //
-//        Optional<User> maybeUser = userService.authenticate(USERNAME,EMAIL);
+//        Optional<User> maybeUser = userService.findById(1);
 //
 //        Assert.assertNotNull(maybeUser);
 //        Assert.assertTrue(maybeUser.isPresent());
 //        Assert.assertEquals(USERNAME, maybeUser.get().getUsername());
 //        Assert.assertEquals(EMAIL, maybeUser.get().getEmail());
+//        Assert.assertEquals(PASSWORD,maybeUser.get().getPassword());
 //        Assert.assertEquals(1L, maybeUser.get().getId());
-//        Mockito.verify(mockDao).authenticate(USERNAME,EMAIL);
-//        Mockito.verifyNoMoreInteractions(mockDao);
 //    }
-//
+
+    /// Redundante
 //    @Test
-//    public void testAuthenticateNonExistent(){
-//        Mockito.when(mockDao.authenticate(USERNAME,EMAIL)).thenReturn(Optional.empty());
+//    public void testFindNonExistent(){
+//        Mockito.when(mockUserDao.findById(1)).thenReturn(Optional.empty());
 //
-//        Optional<User> maybeUser = userService.authenticate(USERNAME,EMAIL);
+//        Optional<User> maybeUser = userService.findById(1);
 //
 //        Assert.assertNotNull(maybeUser);
-//        Assert.assertFalse(maybeUser.isPresent());
-//        Mockito.verify(mockDao).authenticate(USERNAME,EMAIL);
-//        Mockito.verifyNoMoreInteractions(mockDao);
+//        Assert.assertTrue(maybeUser.isEmpty());
 //    }
 }
