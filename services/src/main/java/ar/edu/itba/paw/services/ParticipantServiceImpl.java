159db0cec107f123a0dce44f6f17546e2fd78146
isa-conde <isconde@itba.edu.ar>
2025-11-14 22:57:43 -0300

    fix merge

--- services/src/main/java/ar/edu/itba/paw/services/ParticipantServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/ParticipantServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 @Service
@@ -175,11 +176,7 @@ public class ParticipantServiceImpl implements ParticipantService {
     @Transactional
     @Override
     public void removeParticipant(Long tournamentId, Long participantId){
-        Optional<Tournament> optionalTournament = tournamentDao.findById(tournamentId);
-        if(optionalTournament.isEmpty()){
-            return;
-        }
-        Tournament tournament = optionalTournament.get();
+        Tournament tournament = tournamentDao.findById(tournamentId).orElseThrow(TournamentNotFoundException::new);
         if(!tournament.getOpenInscriptions()){
             LOGGER.warn("Cannot remove participant from a closed tournament");
             return;
