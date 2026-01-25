52fc3f763c7e488dcb9a0bf0582458525cb3a56c
BrunTac <152937106+BrunTac@users.noreply.github.com>
2026-01-25 12:44:43 +0900

    chore: debugged some aspects of the filter

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtAuthenticationFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtAuthenticationFilter.java
@@ -1,9 +1,12 @@
 package ar.edu.itba.paw.webapp.auth;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,12 +14,13 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
 import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final static int JWS_STRING_POS = 1;
+    private static final String BEARER_PREFIX = "Bearer ";
 
     @Autowired
     private JwtTokenService jwtTokenService;
@@ -24,10 +28,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
-        if(authHeader.startsWith("Bearer ")) {
-            String jws = authHeader.split(" ")[JWS_STRING_POS];
-            UserDetails userDetails = jwtTokenService.validateJwsToken(jws);
-            Authentication token = new UsernamePasswordAuthenticationToken(userDetails, null);
+        if(authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
+            String jws = authHeader.substring(BEARER_PREFIX.length()).trim();
+            UserDetails userDetails = null;
+            try {
+                userDetails = jwtTokenService.validateJwsToken(jws);
+            } catch (JwtException e) {
+                SecurityContextHolder.clearContext();
+                filterChain.doFilter(request, response);
+            }
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            if(securityContext.getAuthentication() == null) {
+                securityContext.setAuthentication(authentication);
+            }
         }
         filterChain.doFilter(request, response);
     }
