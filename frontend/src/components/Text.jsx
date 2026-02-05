f66c11bc8fe6957c1e9adb5b82c183ec37082645
BrunTac <152937106+BrunTac@users.noreply.github.com>
2026-02-05 18:37:33 -0300

    chore: added Text component and its styling

--- frontend/src/components/Text.jsx
+++ frontend/src/components/Text.jsx
@@ -1,9 +1,12 @@
-export const Text = ({ type, size, weight, stroke }) => {
-  type = type || "main-text";
-  size = size || "m";
-  weight = weight || "bold";
-  stroke = stroke || false;
+import styles from "../styles/Text.module.css";
 
+export const Text = ({ type, size, weight, stroke, children }) => {
+  const textType = type || "main-text";
+  const fontSize = size || "m";
+  const fontWeight = weight || "bold";
+  const strokeClass = stroke ? styles["text-stroke"] : "";
 
+  const className = `${styles[textType]} ${styles[fontSize]} ${styles[fontWeight]} ${strokeClass}`;
+
+  return <p className={className}>{children}</p>;
 };
-
