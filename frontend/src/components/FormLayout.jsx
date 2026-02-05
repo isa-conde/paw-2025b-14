fc1adec8cc573011ab15bf1cc3aa174219dc9a81
BrunTac <152937106+BrunTac@users.noreply.github.com>
2026-02-05 18:14:30 -0300

    chore: added FormLayout styling

--- frontend/src/components/FormLayout.jsx
+++ frontend/src/components/FormLayout.jsx
@@ -4,11 +4,11 @@ import styles from "../styles/Form.module.css";
 
 export const FormLayout = ({ pageTitle, children }) => {
   return (
-    <>
+    <div className={`${styles.page}`}>
         <Helmet>
             <title>{pageTitle ? `RankUp - ${pageTitle}` : "RankUp"}</title>
         </Helmet>
         {children}
-    </>
+    </div>
   );
 };
---
+++ frontend/src/components/Text.jsx
@@ -0,0 +1,9 @@
+export const Text = ({ type, size, weight, stroke }) => {
+  type = type || "main-text";
+  size = size || "m";
+  weight = weight || "bold";
+  stroke = stroke || false;
+
+
+};
+
