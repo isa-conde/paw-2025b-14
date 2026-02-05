21dfaa31718d035936dfb766986a80ec79685a07
BrunTac <152937106+BrunTac@users.noreply.github.com>
2026-02-05 19:51:03 -0300

    chore: set up internationalization with i18next

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/app.css';
+import './i18n';
 
 const rootElement = document.getElementById('root');
 
 createRoot(rootElement).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Suspense>
     </React.StrictMode>
 );
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import Text from "../components/Text.jsx"
+import FormLayout from "../components/FormLayout.jsx"
+
+export const LoginPage = () => {
+    return {
+        <FormLayout>
+
+        </FormLayout>
+    }
+}
