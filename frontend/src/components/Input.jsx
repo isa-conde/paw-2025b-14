19c8d03ef26b8e841b3f17f58f913b47857cce44
BrunTac <152937106+BrunTac@users.noreply.github.com>
2026-02-06 15:09:56 -0300

    chore: added submit input type to Input component

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -9,6 +9,7 @@ export const Input = ({   id,
                           value,
                           disabled,
                           placeholder,
+                          secondary = false,
                           error,
                           onChange }) => {
     const containerClass = inline
@@ -17,24 +18,39 @@ export const Input = ({   id,
         ? styles.halfContainer
         : styles.container
 
-    if(type === "input") {
+    if(type !== "submit") {
+        if(type === "input") {
+            return (
+                <label htmlFor={id} className={`${styles.label} ${containerClass}`}>
+                    {label && (
+                        <Text size="l" weight="semi-bold">{label}</Text>
+                    )}
+                    <input
+                        id={id}
+                        name={name ?? id}
+                        type="text"
+                        className={styles.input}
+                        value={value}
+                        placeholder={placeholder}
+                        disabled={disabled}
+                        onChange={onChange}
+                    />
+                    {error && <p className={styles.error}>{error}</p>}
+                </label>
+            )
+        }
+    } else {
+        const submitBtnClassName = [ styles.btnSubmit,
+            secondary
+                ? styles.secondary
+                : "",
+        ].filter(Boolean).join(" ");
+
         return (
-            <label htmlFor={id} className={`${styles.label} ${containerClass}`}>
-                {label && (
-                    <Text size="l" weight="semi-bold">{label}</Text>
-                )}
-                <input
-                id={id}
-                name={name ?? id}
-                type="text"
-                className={styles.input}
-                value={value}
-                placeholder={placeholder}
-                disabled={disabled}
-                onChange={onChange}
-                />
-                {error && <p className={styles.error}>{error}</p>}
-            </label>
-        )
+            <div className={`${styles.inlineContainer} ${styles.submitContainer}`}>
+                <input type="submit" className={submitBtnClassName} value={label}/>
+            </div>
+        );
     }
+
 }
